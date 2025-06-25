6ed40ab3b24aee9d4d0ac964ec2295e9a7d4674a
해담 강 <goekazld123@naver.com>
2025-06-25 16:13:10 +0900

    feat: 18870_S2_좌표압축 문제 풀이 완료

--- Baekjoon/18870_S2_좌표 압축.cpp
+++ Baekjoon/18870_S2_좌표 압축.cpp
@@ -1,14 +1,18 @@
 #include<iostream>
 #include<map>
 #include<vector>
+#include<algorithm>
 
 using namespace std;
 
-map<int, int> result;
-vector<int> inputs;
+vector<int> input;
+vector<int> v;
 
 int main()
 {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
 	int N;
 	std::cin >> N;
 
@@ -17,19 +21,52 @@ int main()
 		int num;
 		std::cin >> num;
 
-		inputs.push_back(num);
-
-		if (result[num]) continue;
-		else result[num] = 0;
+		input.push_back(num);
+		v.push_back(num);
 	}
 
-	int index = 0;
-	for (auto num : result)
+	sort(v.begin(), v.end());
+	v.erase(unique(v.begin(), v.end()), v.end());
+
+	for (auto& num : input)
 	{
-		result[num.first] = index;
-		index++;
+		cout << lower_bound(v.begin(), v.end(), num) - v.begin() << " ";
 	}
 
-	for (auto& num : inputs)
-		cout << result[num] << " ";
+	return 0;
 }
+
+//내 처음 풀이 -> map을 사용하니까 속도가 훨씬 느리다..
+//map<int, int> result;
+//vector<int> inputs;
+//
+//int main()
+//{
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(NULL); cout.tie(NULL);
+//	int N;
+//	std::cin >> N;
+//
+//	for (int i = 0; i < N; i++)
+//	{
+//		int num;
+//		std::cin >> num;
+//
+//		inputs.push_back(num);
+//
+//		if (result[num]) continue;
+//		else result[num] = 0;
+//	}
+//
+//	int index = 0;
+//	for (auto num : result)
+//	{
+//		result[num.first] = index;
+//		index++;
+//	}
+//
+//	for (auto& num : inputs)
+//		cout << result[num] << " ";
+//}
+
+
