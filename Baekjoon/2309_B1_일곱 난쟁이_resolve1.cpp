52897154c5dfb2aff00ccebc1230ffe61c7cf5b2
marshmar <goekazld123@naver.com>
2025-07-05 16:40:10 +0900

    feat: 알고리즘 문제 풀이_25_07_25
    - 2308_B1_일곱 난쟁이 재풀이

---
+++ Baekjoon/2309_B1_일곱 난쟁이_resolve1.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int n = 9, k = 7;
+vector<int> v(9);
+
+void print()
+{
+	for (int i = 0; i < 7; i++)
+		cout << v[i] << endl;
+
+	return;
+}
+
+void solve()
+{
+	int sum = 0;
+	
+	for (int i = 0; i < 7; i++)
+	{
+		sum += v[i];
+	}
+
+	if (sum == 100)
+	{
+		print();
+		exit(0);
+	}
+	return;
+}
+
+void combi()
+{
+
+}
+
+int main()
+{
+	for (int i = 0; i < 9; i++)
+		cin >> v[i];
+
+	sort(v.begin(), v.end());
+	do
+	{
+		solve();
+	} while (next_permutation(v.begin(), v.end()));
+	return 0;
+}
