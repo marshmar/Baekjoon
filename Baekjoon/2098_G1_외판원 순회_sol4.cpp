777ab95bfdaa811ccb5b6fca826d570f9be02ff4
marshmar <goekazld123@naver.com>
2025-10-10 15:59:20 +0900

    feat: 알고리즘 문제 풀이_25_10_10

---
+++ Baekjoon/2098_G1_외판원 순회_sol4.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+const int ms = 16, INF = 987654321;
+int n, w[ms][ms], dp[ms][1 << ms];
+
+int tsp(int cur, int visited)
+{
+	if (visited == (1 << n) - 1)
+	{
+		return w[cur][0] ? w[cur][0] : INF;
+	}
+
+	int& ret = dp[cur][visited];
+	if (ret != -1) return ret;
+
+	ret = INF;
+	for (int i = 0; i < n; i++)
+	{
+		if (visited & (1 << i)) continue;
+		if (w[cur][i] == 0) continue;
+		ret = min(ret, tsp(i, visited | (1 << i)) + w[cur][i]);
+	}
+	return ret;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			cin >> w[i][j];
+
+	memset(dp, -1, sizeof(dp));
+	cout << tsp(0, 1) << "\n";
+	return 0;
+}
