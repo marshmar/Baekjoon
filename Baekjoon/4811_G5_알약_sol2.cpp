ae7b3bb6364b9255ba03e8462f3999abf7f8fe17
marshmar <goekazld123@naver.com>
2025-10-13 16:47:02 +0900

    feat: 알고리즘 문제 풀이_25_10_13

---
+++ Baekjoon/4811_G5_알약_sol2.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+using ll = long long;
+
+const int mt = 1004, mw = 34;
+ll n, dp[mw][mw];
+
+ll solve(int wCnt, int hCnt)
+{
+	if (wCnt == 0 && hCnt == 0) return 1;
+
+	ll& ret = dp[wCnt][hCnt];
+	if (ret) return ret;
+
+	if(wCnt > 0) ret += solve(wCnt - 1, hCnt + 1);
+	if (hCnt > 0) ret += solve(wCnt, hCnt - 1);
+
+	return ret;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	while (true)
+	{
+		cin >> n;
+		if (!n) break;
+
+		cout << solve(n, 0) << "\n";
+	}
+	return 0;
+}
