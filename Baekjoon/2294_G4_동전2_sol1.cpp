b4a0d9718d155d75f4f9adcce22685425bfb728f
marshmar <goekazld123@naver.com>
2025-10-19 12:06:38 +0900

    feat: 알고리즘 문제 풀이_25_10_19

---
+++ Baekjoon/2294_G4_동전2_sol1.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+const int ms = 10'004, INF = 987654321;
+
+int n, k, dp[ms];
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n >> k;
+	fill(dp, dp + ms, INF);
+	dp[0] = 0;
+
+	int temp;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> temp;
+		for (int j = temp; j <= k; j++)
+		{
+			dp[j] = min(dp[j], dp[j - temp] + 1);
+		}
+	}
+
+	if (dp[k] == INF) cout << -1 << "\n";
+	else cout << dp[k] << "\n";
+
+	return 0;
+}
